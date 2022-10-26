191287a39a2d329b68606ddcb856ea64a3c5f8c5
Philip Johansson <pjohansson76@yahoo.com>
2022-10-26 17:18:04 +0200

    -- test

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,4 +1,4 @@
 <div class="mb-6">
-    <input name="{{ $name }}" id="{{ $id or $name }}" {{ $attributes->merge(["class" => "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]) }}>
+    <input name="{{ $name }}" id="{{ isset($id) ? $id : $name }}" {{ $attributes->merge(["class" => "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]) }}>
     <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-300 hidden">{{ $slot }}</label>
 </div>
