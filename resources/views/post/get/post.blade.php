8407c01bc755d401debc57da9dcd22e0bc83a37a
Philip Johansson <pjohansson76@yahoo.com>
2022-10-26 16:53:58 +0200

    -- test

--- resources/views/post/get/post.blade.php
+++ resources/views/post/get/post.blade.php
@@ -1,4 +1,5 @@
-<div id="post-{{ $post->id }}" data-post="{{ $post->id }}" class="w-full px-5 py-2 relative border-slate-800 bg-slate-600 border border-slate-700 rounded-md">
+<div id="post-{{ $post->id }}" data-post="{{ $post->id }}"
+     class="w-full px-5 py-2 relative border-slate-800 bg-slate-600 border border-slate-700 rounded-md">
     <span class="post-creator absolute right-5">{{ $post->username }}</span>
     <div class="mb-2">
         <div class="mb-2 text-xl">
@@ -13,10 +14,10 @@
         <a class="post-load-comments flex flex-row items-center cursor-pointer">
             <span>{{ $post->numberOfComments }}</span>
             <span class="chevron-up hidden">
-                @includeIf("components.icons.mini.chevronUp")
+                @includeIf("Components.icons.mini.chevronUp")
             </span>
             <span class="chevron-down">
-                @includeIf("components.icons.mini.chevronDown")
+                @includeIf("Components.icons.mini.chevronDown")
             </span>
         </a>
     </div>
