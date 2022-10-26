36299f087c8d4aa41c6ffd07f7b4ed3812a55ac9
Philip Johansson <pjohansson76@yahoo.com>
2022-10-26 17:19:38 +0200

    -- test

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,3 @@
 <div class="mb-6">
-    <textarea name="{{ $name }}" id="{{ $id != '' ? $id : $name }}" {{ $attributes->merge(["class" => "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]) }}></textarea>
+    <textarea name="{{ $name }}" id="{{ $id ?? $name }}" {{ $attributes->merge(["class" => "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]) }}></textarea>
 </div>
