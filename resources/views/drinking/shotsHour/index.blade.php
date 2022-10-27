9c2edad278b8f5e3899b5c7f2d0807c9dafb1d2c
Mutari <pjohansson01@yahoo.com>
2022-10-27 23:08:29 +0200

    added 1 hour shot game

--- resources/views/drinking/shotsHour/index.blade.php
+++ resources/views/drinking/shotsHour/index.blade.php
@@ -9,8 +9,11 @@
         </div>
         <div id="game" class="hidden">
 
+            <h1 id="counter" class="font-bold text-8xl">1 hour</h1>
 
-            <h1 class="font-bold text-8xl">1 hour</h1>
+            <div class="fixed bottom-0 right-0 p-4">
+                <span id="shotCounter">0/60 shots</span>
+            </div>
 
         </div>
     </div>
@@ -18,6 +21,10 @@
     <script>
 
         onReady(() => {
+
+            const maleDeepCountdown = new Audio('/sound/male-deep-voice-countdown.wav');
+            const softCountdown = new Audio('/sound/soft-bell-countdown.wav');
+            const iFeelGoodSong = new Audio('/sound/I_Feel_Good.mp3');
 
             let menuPanel = document.querySelector('#gameMenu');
             let gamePanel = document.querySelector('#game');
@@ -33,12 +40,69 @@
 
             function startGame() {
 
+                let counterElement = document.querySelector('#counter');
+
+                let secCounter = 10;
                 let startTimer = setInterval(() => {
 
+                    if(secCounter == 10)
+                        maleDeepCountdown.play();
 
+                    if(secCounter >= 0)
+                        counterElement.innerText = `${secCounter}`
 
+                    if(secCounter < 0) {
+                        counterElement.innerText = 'first shot!';
+                        clearInterval(startTimer);
+                        startShots();
+                    }
+
+                    secCounter--;
                 }, 1000);
 
+            }
+
+            function startShots() {
+
+                let counterElement = document.querySelector('#counter');
+                let shotCounterElement = document.querySelector('#shotCounter');
+
+                const maxShots = 60;
+                let shotCounter = 0;
+                let secCounter = 0;
+
+                let timer = setInterval(() => {
+
+                    if(shotCounter == 0 && secCounter == 0)
+                        counterElement.innerText = 'first shot!';
+                    else if(secCounter == 0)
+                        counterElement.innerText = 'shot!';
+                    else
+                        counterElement.innerText = `${60 - secCounter} sec`
+
+                    if((60 - secCounter) == 2)
+                        softCountdown.play();
+
+                    secCounter++;
+
+                    if(secCounter > 60) {
+                        shotCounterElement.innerText = `${++shotCounter}/${maxShots} shots`;
+                        secCounter = 0;
+                    }
+
+                    if(shotCounter >= maxShots) {
+                        clearInterval(timer);
+                        finished();
+                    }
+
+                }, 1000)
+
+            }
+
+            function finished() {
+                let counterElement = document.querySelector('#counter');
+                counterElement.innerText = 'All done fuckers!!!';
+                iFeelGoodSong.play();
             }
 
         })
